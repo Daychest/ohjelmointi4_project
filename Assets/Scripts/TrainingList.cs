b2fa8604f99d19cdbc239b7f7e31591900d4cb80
Daychest <veikka.paivarinta@gmail.com>
2026-04-27 06:17:37 +0300

    adding new training works

--- Assets/Scripts/TrainingList.cs
+++ Assets/Scripts/TrainingList.cs
@@ -9,6 +9,9 @@ public class TrainingList : MonoBehaviour
     public List<GameObject> trainingButtons = new List<GameObject>();
     public GameObject addTrainingButton;
 
+    public GameObject nameToCopy;
+    public GameObject weekdayImageToCopy;
+
     private const float BUTTON_SPACING = 3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -34,14 +37,14 @@ public class TrainingList : MonoBehaviour
         addTrainingButton.transform.position = lastTrainingButton.transform.position - new Vector3(0, BUTTON_SPACING, 0);
     }
 
-    public void addTraining(GameObject weekdayImage, string name)
+    public void addTraining()
     {
         GameObject newTrainingButton = Instantiate(trainingButtonPrefab, transform);
 
         var images = newTrainingButton.GetComponentsInChildren<Image>();
-        images[images.Length - 1].sprite = weekdayImage.GetComponent<Image>().sprite;
+        images[images.Length - 1].sprite = weekdayImageToCopy.GetComponent<Image>().sprite;
 
-        newTrainingButton.GetComponentInChildren<TMP_Text>().text = name;
+        newTrainingButton.GetComponentInChildren<TMP_Text>().text = nameToCopy.GetComponent<TMP_Text>().text;
 
         trainingButtons.Add(newTrainingButton);
 
