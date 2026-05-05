8f94e85386a56eb589804b6b5fca9f83914b8409
Daychest <veikka.paivarinta@gmail.com>
2026-05-06 00:19:47 +0300

    all friend buttons work

---
+++ Assets/Scripts/FriendButton.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FriendButton : MonoBehaviour
+{
+    public GameObject nameText;
+    public GameObject statusIcon;
+    public Sprite absentSprite;
+
+    public void LoadFriendPage(GameObject friendPage)
+    {
+        FriendProfilePage profilePage = friendPage.GetComponent<FriendProfilePage>();
+        profilePage.nameText.GetComponent<TMP_Text>().text = nameText.GetComponent<TMP_Text>().text;
+        profilePage.statusButtonIcon.GetComponent<Image>().sprite = statusIcon.GetComponent<Image>().sprite;
+
+        if (statusIcon.GetComponent<Image>().sprite == absentSprite)
+        {
+            profilePage.statusButtonText.GetComponent<TMP_Text>().text = "Ei ole salilla";
+        }
+        else
+        {
+            profilePage.statusButtonText.GetComponent<TMP_Text>().text = "On salilla";
+        }
+    }
+}
---
+++ Assets/Scripts/FriendProfilePage.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class FriendProfilePage : MonoBehaviour
+{
+    public GameObject nameText;
+    public GameObject statusButtonIcon;
+    public GameObject statusButtonText;
+}
