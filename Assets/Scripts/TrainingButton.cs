423103035907fca9e10d5544554ce93b5e4fb2ab
Daychest <veikka.paivarinta@gmail.com>
2026-05-06 07:54:00 +0300

    save

---
+++ Assets/Scripts/TrainingButton.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Exercise
+{
+    string name;
+    string weekDay;
+    int sets;
+    int repeats;
+}
+
+public class TrainingButton : MonoBehaviour
+{
+    public GameObject exerciseButtonPrefab;
+    public GameObject repeatBoxButtonPrefab;
+
+    public List<GameObject> exerciseButtons = new List<GameObject>();
+    public List<GameObject> repeatBoxButtons = new List<GameObject>();
+    public List<Exercise> exercises = new List<Exercise>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadEditTraining()
+    {
+
+
+
+    }
+}
