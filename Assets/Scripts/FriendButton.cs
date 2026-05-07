d1afe2beeb7768b555f1baf9ec9b597ac829e5c1
Daychest <veikka.paivarinta@gmail.com>
2026-05-07 18:13:54 +0300

    friend button commented

--- Assets/Scripts/FriendButton.cs
+++ Assets/Scripts/FriendButton.cs
@@ -8,21 +8,29 @@ public class FriendButton : MonoBehaviour
     public GameObject statusIcon;
     public Sprite absentSprite;
 
+    //Prepares the friend profile page according to which friend's button was pressed
     public void LoadFriendPage(GameObject friendPage)
     {
-        FriendProfilePage profilePage = friendPage.GetComponent<FriendProfilePage>();
-        profilePage.nameText.GetComponent<TMP_Text>().text = nameText.GetComponent<TMP_Text>().text;
-        profilePage.statusButtonIcon.GetComponent<Image>().sprite = statusIcon.GetComponent<Image>().sprite;
+        //Get the FriendProfilePage script from the page, so we can set the contents of elements 
+        FriendProfilePage friendProfilePage = friendPage.GetComponent<FriendProfilePage>();
 
+        //Set the displayed friend name to the same as the name on the button
+        friendProfilePage.nameText.GetComponent<TMP_Text>().text = nameText.GetComponent<TMP_Text>().text;
+
+        //Set the displayed gym status sprite (v or x) to the same as on the button
+        friendProfilePage.statusButtonIcon.GetComponent<Image>().sprite = statusIcon.GetComponent<Image>().sprite;
+
+        //Set the status text based on which one the status sprite was
         if (statusIcon.GetComponent<Image>().sprite == absentSprite)
         {
-            profilePage.statusButtonText.GetComponent<TMP_Text>().text = "Ei ole salilla";
+            friendProfilePage.statusButtonText.GetComponent<TMP_Text>().text = "Ei ole salilla";
         }
         else
         {
-            profilePage.statusButtonText.GetComponent<TMP_Text>().text = "On salilla";
+            friendProfilePage.statusButtonText.GetComponent<TMP_Text>().text = "On salilla";
         }
 
+        //Mark down which friend button was pressed so we can delete it using the delete friend button
         transform.parent.gameObject.GetComponent<FriendButtonList>().activeFriendButton = gameObject;
     }
 }
